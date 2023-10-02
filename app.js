81ea2572884aad45eda056068c0496ebcbbd8456
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2023-10-02 22:13:50 +1100

    successfully refactored to class for OOP

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-let dataArray = [];
-
 class Game {
   constructor() {
     this.dataArray = [];
   }
+
+  async getData() {
+    const results = await fetch("https://restcountries.com/v3.1/all", {
+      mode: "cors",
+    });
+    const betterResults = await results.json();
+    this.dataArray = betterResults;
+  }
+
+  randomNumber(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  async pickARandomCountry() {
+    const myRandomNumber = this.randomNumber(this.dataArray.length);
+    const randomCountry = this.dataArray.splice(myRandomNumber, 1);
+    return randomCountry;
+  }
+
+  async playGame() {
+    await this.getData();
+    const actualCountry = await this.pickARandomCountry();
+    const secondCountry = await this.pickARandomCountry();
+    const thirdCountry = await this.pickARandomCountry();
+
+    console.log(actualCountry[0].name.common),
+      console.log(
+        actualCountry[0].capital[0],
+        "|",
+        secondCountry[0].capital[0],
+        "|",
+        thirdCountry[0].capital[0]
+      );
+  }
 }
 
-async function getData() {
-  const results = await fetch("https://restcountries.com/v3.1/all", {
-    mode: "cors",
-  });
-  const betterResults = await results.json();
-  dataArray = betterResults;
-}
+const newGame = new Game();
 
-function randomNumber(max) {
-  return Math.floor(Math.random() * max);
-}
-
-async function pickARandomCountry() {
-  const myRandomNumber = randomNumber(dataArray.length);
-  const randomCountry = dataArray.splice(myRandomNumber, 1);
-  return randomCountry;
-}
-
-async function playGame() {
-  await getData();
-  const actualCountry = await pickARandomCountry();
-  const secondCountry = await pickARandomCountry();
-  const thirdCountry = await pickARandomCountry();
-
-  console.log(actualCountry[0].name.common),
-    console.log(
-      actualCountry[0].capital[0],
-      "|",
-      secondCountry[0].capital[0],
-      "|",
-      thirdCountry[0].capital[0]
-    );
-}
-
-playGame();
+newGame.playGame();
